b0c85ad503e7683c128963fc8a9540bfd37bc40a
Marc Theunissen <emtyr@protonmail.com>
2023-03-29 12:02:21 +0200

    feat: rename consumer variables

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,3 @@
-import { Consumer } from 'sqs-consumer';
-
 import { WordStorage } from './word-storage.js'
 import { Worker } from './worker.js';
 import { WordPortionPosition } from './word.js';
@@ -8,12 +6,12 @@ import { createWordPortionConsumer } from './consumer.js';
 const storage = new WordStorage();
 const worker = new Worker(storage);
 
-const queueWpBegin = createWordPortionConsumer(storage, WordPortionPosition.begin);
-const queueWpMiddle = createWordPortionConsumer(storage, WordPortionPosition.middle);
-const queueWpEnd = createWordPortionConsumer(storage, WordPortionPosition.end);
+const consumerWordPortionBegin = createWordPortionConsumer(storage, WordPortionPosition.begin);
+const consumerWordPortionMiddle = createWordPortionConsumer(storage, WordPortionPosition.middle);
+const consumerWordPortionEnd = createWordPortionConsumer(storage, WordPortionPosition.end);
 
 worker.start();
 
-queueWpBegin.start();
-queueWpMiddle.start();
-queueWpEnd.start();
+consumerWordPortionBegin.start();
+consumerWordPortionMiddle.start();
+consumerWordPortionEnd.start();
